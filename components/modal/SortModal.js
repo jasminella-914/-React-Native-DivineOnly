87478d84c91edb78017c94a958c6a8a3482e5726
Philip Cesar Garay <suicodanielarose@gmail.com>
2025-06-24 20:58:29 +0800

    worked the sort modal I added in booking screen

--- components/modal/SortModal.js
+++ components/modal/SortModal.js
@@ -1,15 +1,82 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 import ActionSheet, { SheetManager } from "react-native-actions-sheet";
+import { Switch } from "react-native-gesture-handler";
 
 export default function SortModal() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
   return (
-    <ActionSheet containerStyle={{ flex: 1}} id="Sort">
-      <View>
-        <Text>SortModal</Text>
+    <ActionSheet containerStyle={{ flex: 0.7 }} id="Sort">
+      <View style={{ paddingHorizontal: 20, marginTop: 20 }}>
+        <View></View>
+        <Text style={styles.headerText}>Trier et filtrer</Text>
+        <View style={{ gap: 20 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text>Choisis pour vous</Text>
+            <Switch
+              trackColor={{ false: "#D4D4D4", true: "#469597" }}
+              thumbColor={isEnabled ? "#FFFFFF" : "#FFFFFF"}
+              ios_backgroundColor="#D4D4D4"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text>Disponible aujourd’hui</Text>
+            <Switch
+              trackColor={{ false: "#D4D4D4", true: "#469597" }}
+              thumbColor={isEnabled ? "#FFFFFF" : "#FFFFFF"}
+              ios_backgroundColor="#D4D4D4"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+          </View>
+        </View>
+        <View style={{ marginTop: "auto" }}>
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>Appliquer</Text>
+          </Pressable>
+          <Pressable>
+            <Text style={[styles.buttonText, { color: "#469597" }]}>
+              Réinitialiser
+            </Text>
+          </Pressable>
+        </View>
       </View>
     </ActionSheet>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headerText: {
+    fontSize: 18,
+    fontWeight: "700",
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#469597",
+    paddingVertical: 15,
+    paddingHorizontal: 114,
+    borderRadius: 10,
+    marginTop: "auto",
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "600",
+    textAlign: "center",
+  },
+});
