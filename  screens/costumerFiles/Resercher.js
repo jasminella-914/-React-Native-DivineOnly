b84b63c734dd097371dd7bcbfd11c28c1a842fe1
Philip Cesar Garay <216655526+jasminella-914@users.noreply.github.com>
2025-07-10 20:04:42 +0800

    in Resercher screen, I imported the lodash filter and  added the flat list component

---  screens/costumerFiles/Resercher.js
+++  screens/costumerFiles/Resercher.js
@@ -7,73 +7,24 @@ import {
   Pressable,
   FlatList,
   StatusBar,
+  TextInput,
+  Image,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { TextInput } from "react-native-gesture-handler";
 import { navigate } from "../../navigationService";
+import { SEARCHLIST_DATA } from "../../data/data";
+import filter from "lodash.filter";
 
 export default function Resercher({ navigation }) {
-  // const [search, setSearch] = useState("");
-  // const [filteredDataSource, setFilteredDataSource] = useState([]);
-  // const [masterDataSource, setMasterDataSource] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
-  // useEffect(() => {
-  //   fetch("https://jsonplaceholder.typicode.com/posts")
-  //     .then((response) => response.json())
-  //     .then((responseJson) => {
-  //       setFilteredDataSource(responseJson);
-  //       setMasterDataSource(responseJson);
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // }, []);
+  const handleSearch = (text) => {
+    setSearchQuery(text);
+  };
 
-  // const searchFilterFunction = (text) => {
-  //   if (text) {
-  //     const newData = masterDataSource.filter(function (item) {
-  //       const itemData = item.title
-  //         ? item.title.toUpperCase()
-  //         : "".toUpperCase();
-  //       const textData = text.toUpperCase();
-  //       return itemData.indexOf(textData) > -1;
-  //     });
-  //     setFilteredDataSource(newData);
-  //     setSearch(text);
-  //   } else {
-  //     setFilteredDataSource(masterDataSource);
-  //     setSearch(text);
-  //   }
-  // };
-
-  // const ItemView = ({ item }) => {
-  //   return (
-  //     <Text style={styles.itemStyle} onPress={() => getItem(item)}>
-  //       {item.id}
-  //       {"."}
-  //       {item.title.toUpperCase()}
-  //     </Text>
-  //   );
-  // };
-
-  // const ItemSeparatorView = () => {
-  //   return (
-  //     <View
-  //       style={{
-  //         height: 0.5,
-  //         width: "100%",
-  //         backgroundColor: "#C8C8C8",
-  //       }}
-  //     />
-  //   );
-  // };
-
-  // const getItem = (item) => {
-  //   alert("Id : " + item.id + " Title : " + item.title);
-  // };
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <View style={styles.header}>
         <Pressable onPress={() => navigation.goBack()}>
           <Ionicons name="close-outline" size={24} />
@@ -81,50 +32,54 @@ export default function Resercher({ navigation }) {
         <Text style={styles.headertext}>Resercher</Text>
       </View>
       <View style={styles.secondcontainer}>
-        <TextInput placeholder="Où ?" style={styles.input} />
-        <Pressable
-        onPress={() => navigate("Booking")}
-        style={{ flexDirection: "row", gap: 5, alignItems: "center" }}
-      >
-        <Ionicons name="location-outline" size={24} color={"#469597"} />
-        <Text style={styles.locText}>Autour de moi</Text>
-      </Pressable> 
-      </View>
-      
-      {/* <View style={styles.secondcontainer}>
-        <View style={{ gap: 10 }}>
-          <Ioniconse         
-            name="search-outline"
-            size={22}
-            color={"#BBC6C8"}
-            style={{ padding: 10 }}
-          />
-          <TextInput placeholder="Où ? " style={styles.input} />
-          <TextInput
-            style={styles.textInputStyle}
-            onChangeText={(text) => searchFilterFunction(text)}
-            value={search}
-            underlineColorAndroid="transparent"
-            placeholder="Où ? "
-          />
+        <TextInput
+          placeholder="Où ?"
+          style={styles.input}
+          clearButtonMode="always"
+          autoCorrect={false}
+          value={searchQuery}
+          onChangeText={(text) => handleSearch(text)}
+        />
 
+        {searchQuery ? (
           <FlatList
-            data={filteredDataSource}
-            keyExtractor={(item, index) => index.toString()}
-            ItemSeparatorComponent={ItemSeparatorView}
-            renderItem={ItemView}
+            data={SEARCHLIST_DATA}
+            renderItem={({ item }) => {
+              return (
+                <Pressable onPress={() => navigate("Booking")} style={styles.listContainer}>
+                  <Image source={item.image} />
+                  <View style={styles.infoContainer}>
+                    <Text style={styles.title}>{item.title}</Text>
+                    <Text style={styles.info}>{item.info}</Text>
+                  </View>
+                </Pressable>
+              );
+            }}
+            ItemSeparatorComponent={() => {
+              return <View style={{ height: 10 }}></View>;
+            }}
+            showsVerticalScrollIndicator={false}
           />
-        </View>
-      </View> */}
-    </SafeAreaView>
+        ) : (
+          <Pressable
+            onPress={() => navigate("Booking")}
+            style={{ flexDirection: "row", gap: 5, alignItems: "center" }}
+          >
+            <Ionicons name="location-outline" size={24} color={"#469597"} />
+            <Text style={styles.locText}>Autour de moi</Text>
+          </Pressable>
+        )}
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "# ",
-    paddingTop: StatusBar.currentHeight,
+    backgroundColor: "#FFFFFF",
+    paddingTop: 60
+
   },
   headertext: {
     fontSize: 18,
@@ -135,7 +90,7 @@ const styles = StyleSheet.create({
     top: 0,
   },
   header: {
-    marginBottom: 20,
+    marginBottom: 10,
     marginHorizontal: 20,
   },
   secondcontainer: {
@@ -173,5 +128,25 @@ const styles = StyleSheet.create({
 
     backgroundColor: "#FFFFFF",
     borderRadius: 14,
+  },
+  listContainer: {
+    flexDirection: "row",
+    gap: 14,
+    backgroundColor: "#4695971A",
+    paddingVertical: 9,
+    paddingHorizontal: 12,
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  infoContainer: {
+    gap: 8,
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: "500",
+  },
+  info: {
+    fontSize: 12,
+    fontWeight: "400",
   },
 });
